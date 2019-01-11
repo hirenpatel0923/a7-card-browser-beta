84c14302fb19da91d85e983928d662417b2326db
Hiren Patel <hirenpatel0923@gmail.com>
2019-01-11 20:55:27 +0530

    initial setup for material design

---
+++ src/app/material.ts
@@ -0,0 +1,8 @@
+import { MatButtonModule, MatIconModule } from '@angular/material';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    imports: [MatButtonModule, MatIconModule],
+    exports: [MatButtonModule, MatIconModule],
+})
+export class MaterialModule {}
